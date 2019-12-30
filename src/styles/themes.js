6f406d2c980d5161bbccbf042fd7817fd6d6f5d8
jscodelover <manisha.4918@gmail.com>
2019-12-30 19:40:25 +0530

    style: added assets for layout and logic modal

--- src/styles/themes.js
+++ src/styles/themes.js
@@ -1,12 +1,18 @@
 const colors = {
-  black: 'rgba(0,0,0,0.85)',
-  lightgrey: '#f5f5f5',
-  grey: '#9e9e9e'
-}
+	black: 'rgba(0,0,0,0.85)',
+	lightgrey: '#f5f5f5',
+	grey: '#9e9e9e',
+	textColor: '#1A1818',
+	btnText: '#872525',
+	btnTextHover: '#BE2D2D',
+	white: '#ffffff',
+	layoutColor1: '#ffffff',
+	layoutColor2: '#FFC3A0'
+};
 
 export default {
-  colors,
-  shadow: '0 5px 15px rgba(0,0,0,.05)',
-  shadowLight: '0 1px 2px rgba(0, 0, 0, 0.05)',
-  transition: 'all 0.2s ease-in-out'
-}
+	colors,
+	shadow: '0 5px 15px rgba(0,0,0,.05)',
+	shadowLight: '0 1px 2px rgba(0, 0, 0, 0.05)',
+	transition: 'all 0.2s ease-in-out'
+};
